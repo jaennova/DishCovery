d66c57fd058f026e48eb4f1fd7d1cf7af60debce
Jaen <jaennova9@gmail.com>
2025-05-14 23:55:03 -0600

    Initial commit

---
+++ app/src/main/java/com/jaennova/dishcovery/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jaennova.dishcovery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.jaennova.dishcovery.ui.theme.DishcoveryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DishcoveryTheme {
+
+            }
+        }
+    }
+}
