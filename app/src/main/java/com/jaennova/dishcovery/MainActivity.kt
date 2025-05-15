897e516ee1a6ddbd8f334807da5d8e168c6b1994
Jaen <jaennova9@gmail.com>
2025-05-15 09:53:24 -0600

    feat: add cofig navigation in MainActivity

--- app/src/main/java/com/jaennova/dishcovery/MainActivity.kt
+++ app/src/main/java/com/jaennova/dishcovery/MainActivity.kt
@@ -3,17 +3,33 @@ package com.jaennova.dishcovery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.jaennova.dishcovery.ui.screens.RecipeScreen
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.recipesapp.ui.RecipesNavGraph
 import com.jaennova.dishcovery.ui.theme.DishcoveryTheme
+import com.jaennova.dishcovery.viewmodel.RecipeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DishcoveryTheme {
-                RecipeScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    val viewModel: RecipeViewModel = viewModel()
+
+                    RecipesNavGraph(
+                        navController = navController,
+                        viewModel = viewModel
+                    )
+                }
             }
         }
     }
